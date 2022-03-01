49d840b0644134c2c43567b47261e313f42e4878
CHRIS_LEE <chrismlee87@gmail.com>
2022-02-28 19:30:06 -0800

    [add] backend for incoming socket connections

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 //App.js
 const express = require('express');
 const app = express();
-//Socket.io has to use the http server
 const server = require('http').Server(app);
+
+// Socket.io Config
+const io = require('socket.io')(server)
+io.on('connection', (socket) => {
+  console.log('New user connected!')
+})
 
 //Express View Engine for Handlebars
 const exphbs = require('express-handlebars');
